fd9c2ac5b6ca727f02a3b66eaf74f41885d8196d
yindaheng98 <yindaheng98@gmail.com>
2022-04-29 18:53:58 +0800

    基本格式优化

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ async function sendSubscribeTag(msg, match) {
     const chatId = msg.chat.id;
     const msgId = msg.message_id;
     const tag = match[0];
-    console.log(match[0]);
+    console.log(match);
 }
-bot.onText(/^artist:.+$/, sendSubscribeTag);
-bot.onText(/^group:.+$/, sendSubscribeTag);
+bot.onText(/^a:"([^"\$]+)"$/, sendSubscribeTag);
+bot.onText(/^a:"([^"\$]+)\$"$/, sendSubscribeTag);
+bot.onText(/^a:([^"\$\s]+)$/, sendSubscribeTag);
+bot.onText(/^a:([^"\$\s]+)\$$/, sendSubscribeTag);
