3f4669ec0e3f1908fcb9effc0dc11ec3fa53f70c
yindaheng98 <yindaheng98@gmail.com>
2022-04-29 20:29:30 +0800

    use my own command

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = require('rss-telegram-bot/config')
+const conf_prefix = "TH_FEED_FORMAT";
+config.eh_feed_formats = [];
+for (let k in process.env) {
+    if (k.slice(0, conf_prefix.length) === conf_prefix) {
+        config.eh_feed_formats.push(process.env[k]);
+    }
+}
+if (config.eh_feed_formats.length <= 0) {
+    config.eh_feed_formats = ["https://rsshub.app/ehentai/tag/%s"];
+}
--- index.js
+++ index.js
@@ -1,44 +1,36 @@
-const {bot, rss, user} = require('rss-telegram-bot')
-const config = require('rss-telegram-bot/config')
+const { bot, rss, user } = require('rss-telegram-bot')
+const config = require('./config')
 var util = require('util');
 
-config.eh_feed_format = process.env.TH_FEED_FORMAT || "https://rsshub.app/ehentai/tag/%s";
-
-
-async function catInlineKeyboards(url) {
+async function catInlineKeyboards(tag) {
     const inline_keyboards = [];
     const categories = await rss.getCategories();
     for (let category_id in categories) {
         const title = categories[category_id];
         inline_keyboards.push([{
             text: title,
-            switch_inline_query_current_chat: `/subscribe ${category_id} ${url}`
+            switch_inline_query_current_chat: `/subscribe_eh ${category_id} ${tag}`
         }]);
     }
     inline_keyboards.push([{
         text: 'Cancel it',
-        switch_inline_query_current_chat: `/unparse_eh ${url}`
+        switch_inline_query_current_chat: `/unparse_eh ${tag}`
     }]);
     return inline_keyboards
 }
 
-async function sendParseTo(url, msg) {
-    const chatId = msg.chat.id;
-    const msgId = msg.message_id;
-    bot.sendMessage(chatId, `${url}\nPlease select a category to subscribe:`, {
-        reply_to_message_id: msgId,
-        reply_markup: {
-            inline_keyboard: await catInlineKeyboards(url)
-        }
-    });
-}
-
-function sendSubscribeTag(msg, match, tag) {
+async function sendSubscribeTag(msg, match, tag) {
     if (match.length <= 0 || match[1] === null || match[1].length <= 0) {
         return
     }
-    const feed_url = util.format(config.eh_feed_format, tag);
-    sendParseTo(feed_url, msg);
+    const chatId = msg.chat.id;
+    const msgId = msg.message_id;
+    bot.sendMessage(chatId, `${tag}\nPlease select a category to subscribe:`, {
+        reply_to_message_id: msgId,
+        reply_markup: {
+            inline_keyboard: await catInlineKeyboards(tag)
+        }
+    });
 }
 
 function handlerFuncGen(tag_format) {
@@ -59,7 +51,7 @@ function handlerGen(tag_prefix, func) {
     bot.onText(RegExp(`^${tag_prefix}:([^"\\$\\s]+)\\$$`), func);
 }
 
-handlerGen('a', handlerFuncGen('artist%%3A%%22%s%%24%%22'));
-handlerGen('artist', handlerFuncGen('artist%%3A%%22%s%%24%%22"'));
-handlerGen('g', handlerFuncGen('group%%3A%%22%s%%24%%22"'));
-handlerGen('group', handlerFuncGen('group%%3A%%22%s%%24%%22"'));
+handlerGen('a', handlerFuncGen('artist:"%s$"'));
+handlerGen('artist', handlerFuncGen('artist:"%s$"'));
+handlerGen('g', handlerFuncGen('group:"%s$"'));
+handlerGen('group', handlerFuncGen('group:"%s$"'));
