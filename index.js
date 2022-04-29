9d9e20f82e4c36422d00fee217e8672e3af8b635
yindaheng98 <yindaheng98@gmail.com>
2022-04-29 19:31:16 +0800

    链接格式搞定

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 const bot = require('rss-telegram-bot')
 const config = require('rss-telegram-bot/config')
-async function sendSubscribeTag(msg, match) {
+var util = require('util');
+
+config.eh_feed_format = process.env.TH_FEED_FORMAT || "https://rsshub.app/ehentai/tag/%s";
+
+async function sendSubscribeTag(msg, match, tag) {
     const chatId = msg.chat.id;
     const msgId = msg.message_id;
-    const tag = match[0];
-    console.log(match);
+    if (match.length <= 0 || match[1] === null || match[1].length <= 0) {
+        return
+    }
+    const feed_url = util.format(config.eh_feed_format, tag);
+    console.log(feed_url);
 }
 
-function convient(tag_prefix, func) {
+function handlerFuncGen(tag_format) {
+    return async function (msg, match) {
+        if (match.length <= 0 || match[1] === null || match[1].length <= 0) {
+            return
+        }
+        let tag_content = match[1];
+        tag_content = tag_content.replace(" ", "+")
+       await sendSubscribeTag(msg, match, util.format(tag_format, tag_content))
+    }
+}
+
+function handlerGen(tag_prefix, func) {
     bot.onText(RegExp(`^${tag_prefix}:"([^"\\$]+)"$`), func);
     bot.onText(RegExp(`^${tag_prefix}:"([^"\\$]+)\\$"$`), func);
     bot.onText(RegExp(`^${tag_prefix}:([^"\\$\\s]+)$`), func);
     bot.onText(RegExp(`^${tag_prefix}:([^"\\$\\s]+)\\$$`), func);
 }
 
-convient('a', sendSubscribeTag);
-convient('artist', sendSubscribeTag);
-convient('g', sendSubscribeTag);
-convient('group', sendSubscribeTag);
+handlerGen('a', handlerFuncGen('artist:"%s$"'));
+handlerGen('artist', handlerFuncGen('artist:"%s$"'));
+handlerGen('g', handlerFuncGen('group:"%s$"'));
+handlerGen('group', handlerFuncGen('group:"%s$"'));
